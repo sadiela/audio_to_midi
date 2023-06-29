e44288c01a763ee9985f53f8046a1e36bf85189e
sadiela <sadiela@bu.edu>
2023-06-29 11:59:15 -0700

    spectrogram dataloader almost done, working on midi vocab

---
+++ midi_vocabulary.py
@@ -0,0 +1,99 @@
+import os
+import pretty_midi
+from tqdm import tqdm
+
+SEG_LENGTH_SECS = 4.0879375
+BIN_QUANTIZATION = 0.01 # should only need 500 time events
+SOUNDFONT_PATH = "./GeneralUser_GS_v1.471.sf2"
+
+
+event_dictionary = {}
+event_dictionary[0] = '<EOS>'
+event_dictionary[1] = '<PAD>'
+for i in range(2,130):
+    event_dictionary[i] = 'NOTE:' + str(i-2)
+
+for i in range(130,631):
+    event_dictionary[i] = round(0.01*(i-129),2)
+
+event_idxs = {v: k for k, v in event_dictionary.items()}
+
+def seq_chunks_to_pretty_midi(seq_chunks, target_dir):
+    cur_midi = pretty_midi.PrettyMIDI() # define new midi object WITH PROPER TEMPO!!!
+    cur_inst = pretty_midi.Instrument(program=1)
+    for i, chunk in enumerate(seq_chunks):
+        base_time = SEG_LENGTH_SECS*i # whatever chunk we are at
+        cur_time = base_time
+        for event in chunk: 
+            if event in range(2, 130): # NOTE EVENT!
+                note = pretty_midi.Note(velocity=100, pitch=event-2, start=cur_time, end=cur_time + 0.25)
+                cur_inst.notes.append(note)
+            elif event in range(130,631): # TIME SHIFT!
+                cur_time = base_time + event_dictionary[event]
+            elif event == 0:
+                break # end of this chunk
+    
+    cur_midi.instruments.append(cur_inst)
+    if not os.path.exists(target_dir + 'seq_conversion1.mid'):
+        cur_midi.write(str(target_dir + 'seq_conversion1.mid'))
+        
+
+def pretty_midi_to_seq_chunks(open_midi): 
+    note_starts = [note.start for note in open_midi.instruments[0].notes]
+    num_segs = int((note_starts[-1] // SEG_LENGTH_SECS)) + 1
+    event_sequences = [[] for _ in range(num_segs)]
+    cur_seg = 0
+    previous_note_time = 0.0
+    for note in open_midi.instruments[0].notes:
+        print(note)
+        if note.start > SEG_LENGTH_SECS*(cur_seg+1):
+            #print("STARTING NEW SEGMENT")
+            cur_seg = int((note.start // SEG_LENGTH_SECS))
+        if note.start > previous_note_time and note.start > cur_seg*SEG_LENGTH_SECS: 
+            # add a time event
+            note_offset = note.start - cur_seg*SEG_LENGTH_SECS
+            rounded_offset = round(note_offset, 2)
+            #print("OFFSET AND ROUNDED:", note_offset, rounded_offset)
+            #print("DICT EVENT:", event_idxs[rounded_offset])
+            if rounded_offset != 0.0:
+                event_sequences[cur_seg].append(event_idxs[rounded_offset])
+        event_sequences[cur_seg].append(event_idxs["NOTE:"+str(note.pitch)])
+        #print("SEQUENCE:", event_sequences, [event_dictionary[i] for i in event_sequences[cur_seg]])
+        #input("Continue...")
+        previous_note_time = note.start
+    for seq in event_sequences:
+        seq.append(0) # APPEND EOS TOKENS
+        print(len(seq))
+    # NOW PAD THEM ALL TO THE LENGTH OF THE LONGEST ONE!
+    longest_seq = max([len(seq) for seq in event_sequences])
+    print("LONGEST:", longest_seq)
+    for seq in event_sequences:
+        while (len(seq)) < longest_seq:
+            seq.append(1) # PADDING!
+    return event_sequences
+
+def midi_to_wav(midi_path,wav_path):
+    print("CONVERTING")
+    # using the default sound font in 44100 Hz sample rate
+    #cmd = "fluidsynth -F " + wav_path + ' ' + SOUNDFONT_PATH + ' ' + midi_path + ' -r 16000 -i'
+    cmd = "fluidsynth -F " + wav_path + ' ' + SOUNDFONT_PATH + ' ' + midi_path + ' -r 16000 -i'
+    print(cmd)
+    ret_status = os.system(cmd)
+
+
+if __name__ == '__main__':
+    midi_directory = './small_matched_data/midi/'
+    mid_files = os.listdir(midi_directory)
+    target_dir = './small_matched_data/midi_reconverted/'
+
+    # start with just note onset and time events!
+
+    # Test by converting back to MIDI
+    for file in mid_files:
+        print(file)
+        print(midi_directory + file)
+        open_midi = pretty_midi.PrettyMIDI(midi_directory + file)
+        seq_chunks = pretty_midi_to_seq_chunks(open_midi)
+        seq_chunks_to_pretty_midi(seq_chunks, target_dir)
+        midi_to_wav(target_dir + 'seq_conversion1.mid', target_dir + 'seq_conv1.wav')
+        input("Continue...")

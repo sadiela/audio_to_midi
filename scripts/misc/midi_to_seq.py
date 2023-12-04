36a976992646186b430d834a122c0e9d0682e2a9
David Liu <dcliu3@exxide.bu.edu>
2023-12-04 10:28:08 -0500

    sdfsdaf

--- scripts/misc/midi_to_seq.py
+++ scripts/misc/midi_to_seq.py
@@ -5,6 +5,7 @@ sys.path.insert(0,os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from midi_vocabulary import *
 import numpy as np
 import pdb
+import tqdm
 
 # create a script that converts all of the midi files in lmd tracks to numpy sequences
 
@@ -13,13 +14,15 @@ directory = "/projectnb/textconv/sadiela/audio_to_midi/lmd_tracks"
 saveDirectory = "/projectnb/textconv/dcliu3/audio_to_midi/lmd_tracks_seqs"
 # directory = "/scratch2/lmd_tracks/lmd_tracks"
 # saveDirectory = "/scratch2/lmd_tracks/lmd_tracks_seqs"
-for folder in folder_extensions:
+for folder in tqdm.tqdm(folder_extensions):
     # if folder exists in saveDirectory, then skip, otherwise create
     if not os.path.exists(os.path.join(saveDirectory,folder)):
         os.makedirs(os.path.join(saveDirectory,folder))
-    for file in os.listdir(os.path.join(directory,folder)):
+    for file in tqdm.tqdm(os.listdir(os.path.join(directory,folder))):
         if file.endswith(".mid"):
             tempMid = pretty_midi.PrettyMIDI(os.path.join(directory,folder,file))
+            if len(tempMid.instruments) == 0:
+                break
             try:
                 tempMidData = pretty_midi_to_seq_chunks_w_noteoff_and_velocity(tempMid)
             except:
